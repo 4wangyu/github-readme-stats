20d76c95744ff0491f4a7384e413dc3e680a2f33
4wangyu <54412086+4wangyu@users.noreply.github.com>
2020-08-15 22:51:24 +0800

    Update top-languages-fetcher.js

--- src/fetchers/top-languages-fetcher.js
+++ src/fetchers/top-languages-fetcher.js
@@ -73,7 +73,7 @@ async function fetchTopLanguages(username) {
     }, {});
 
   const topLangs = Object.keys(repoNodes)
-    .slice(0, 16)
+    .slice(0, 12)
     .reduce((result, key) => {
       result[key] = repoNodes[key];
       return result;
